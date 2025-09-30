a210676e5b852bce0f9ea0dde1e4aaa1ab834e10
alexandre <alexandre@gutierres.com.br>
2025-09-30 18:00:31 -0300

    Entrega Projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "Symbian OS", 512);       
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");   
+
+Console.WriteLine("\n-----------------------\n");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Iphone iphone = new Iphone("987654321", "iPhone 13", "iOS", 4096);    
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
